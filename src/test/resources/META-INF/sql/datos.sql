968356622c1259d3f15f32cef3833702445678bc
OC18018 <OC18018@ues.edu.sv>
2022-03-23 08:52:07 -0600

    Configuracion inicial para hacer IT

---
+++ src/test/resources/META-INF/sql/datos.sql
@@ -0,0 +1,3 @@
+INSERT INTO estado (nombre,fecha_creacion) VALUES ('prueba','2022-03-01');
+INSERT INTO estado (nombre,fecha_creacion) VALUES ('prueba2','2022-03-02');
+INSERT INTO estado (nombre,fecha_creacion) VALUES ('prueba3','2022-03-03');
