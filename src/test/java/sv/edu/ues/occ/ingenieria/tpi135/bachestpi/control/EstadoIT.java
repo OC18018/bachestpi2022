7f7f0fb0de56d9859336b3804d5329479ff31fa7
OC18018 <OC18018@ues.edu.sv>
2022-03-23 22:53:42 -0600

    EstadoIT metodos passed

--- src/test/java/sv/edu/ues/occ/ingenieria/tpi135/bachestpi/control/EstadoIT.java
+++ src/test/java/sv/edu/ues/occ/ingenieria/tpi135/bachestpi/control/EstadoIT.java
@@ -15,39 +15,147 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import sv.edu.ues.occ.ingenieria.tpi135.bachestpi.resources.entity.Estado;
-
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
 /**
  *
  * @author armandop444
  */
 @ExtendWith(ArquillianExtension.class)
+@TestMethodOrder(OrderAnnotation.class)
 public class EstadoIT {
-    
+
     @Deployment
-    public static WebArchive crearDespliegue(){
+    public static WebArchive crearDespliegue() {
         WebArchive salida = ShrinkWrap.create(WebArchive.class)
                 .addPackage("sv.edu.ues.occ.ingenieria.tpi135.bachestpi.resources.entity")
                 .addAsResource("persistence-arquillian.xml")
                 .addClass(abstractDataAccess.class)
                 .addClass(EstadoBean.class)
-                .addAsResource("META-INF/persistence.xml","META-INF/persistence.xml")
-                .addAsResource("META-INF/sql/datos.sql","META-INF/sql/datos.sql")
-                .addAsWebInfResource(EmptyAsset.INSTANCE,"beans.xml");
+                .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
+                .addAsResource("META-INF/sql/datos.sql", "META-INF/sql/datos.sql")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
         System.out.println(salida.toString(true));
         return salida;
     }
-    
+
     @Inject
     EstadoBean cut;
-    
+
     @Test
-    public void testFindAll(){
+    @Order(1)
+    public void testContar() {
+        System.out.println("\n\n");
+        System.out.println("\n\n");
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("Contar");
+        System.out.println("\n\n");
+        assertNotNull(cut);
+        Long resultado = cut.contar();
+        assertNotNull(resultado);
+        System.out.println("Se encontraron " + resultado + " Resultados");
+        System.out.println("\n\n");
+    }
+
+    @Test
+    @Order(2)
+    public void testCrear() {
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("Crear");
+        System.out.println("\n\n");
+        assertNotNull(cut);
+        Estado nuevo = new Estado();
+        nuevo.setNombre("creado IT");
+        cut.crear(nuevo);
+        Estado esperado = cut.findById(5);
+        System.out.println("Creado id 5 obteniendo nombre " + esperado.getNombre());
+        System.out.println("\n\n");
+        Long contar = cut.contar();
+        System.out.println("Total de Datos actuales: " + contar);
+        System.out.println("\n\n");
+    }
+
+    @Test
+    @Order(3)
+    public void testFindByid() {
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("FindId");
+        System.out.println("\n\n");
+        assertNotNull(cut);
+        Estado nuevo = new Estado();
+        Integer id = 4;
+        Estado Result = cut.findById(id);
+        System.out.println("Se encontro el resultado " + Result.getNombre());
+        System.out.println("\n\n");
+    }
+
+    @Test
+    @Order(4)
+    public void testModificar() {
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("Modificar");
+        System.out.println("\n\n");
+        assertNotNull(cut);
+        int id = 4;
+        Estado nuevo = new Estado();
+        nuevo.setIdEstado(id);
+        nuevo.setNombre("Modificado en IT");
+        Estado sinModificar = cut.findById(id);
+        System.out.println("Antes de Modificar " + sinModificar.getNombre());
+        System.out.println("\n\n");
+        cut.Modificar(nuevo);
+        Estado modificado = cut.findById(id);
+        System.out.println("Paso Modificar " + modificado.getNombre());
+        System.out.println("\n\n");
+    }
+
+    @Test
+    @Order(5)
+    public void testFindAll() {
+        System.out.println("--------------------------------------------------------------");
         System.out.println("findAll");
+        System.out.println("\n\n");
         assertNotNull(cut);
         List<Estado> resultado = cut.findAll();
         assertNotNull(resultado);
         assertTrue(!resultado.isEmpty());
-        System.out.println("La lista posee"+ resultado.size());
+        System.out.println("La lista posee " + resultado.size());
+        System.out.println("\n\n");
     }
-    
+
+    @Test
+    @Order(6)
+    public void testFindRange() {
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("FindRange");
+        System.out.println("\n\n");
+        assertNotNull(cut);
+        int first = 1;
+        int pageSize = 3;
+        List<Estado> resultado = cut.findRange(first, pageSize);
+        assertNotNull(resultado);
+        assertTrue(!resultado.isEmpty());
+        System.out.println("Se encontraron " + resultado.size());
+        System.out.println("\n\n");
+
+    }
+
+    @Test
+    @Order(7)
+    public void testEliminar() {
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("Eliminar");
+        assertNotNull(cut);
+        Estado eliminado = new Estado();
+        eliminado.setIdEstado(2);
+        System.out.println("\n\n");
+        System.out.println("Registros actuales:" + cut.contar());
+        System.out.println("\n\n");
+        cut.eliminar(eliminado);        
+        System.out.println("Eliminado con exito registro actuales:" + cut.contar());
+        System.out.println("\n\n");
+
+    }
+
 }
--- src/test/resources/META-INF/sql/datos.sql
+++ src/test/resources/META-INF/sql/datos.sql
@@ -1,3 +1,4 @@
 INSERT INTO estado (nombre,fecha_creacion) VALUES ('prueba','2022-03-01');
 INSERT INTO estado (nombre,fecha_creacion) VALUES ('prueba2','2022-03-02');
 INSERT INTO estado (nombre,fecha_creacion) VALUES ('prueba3','2022-03-03');
+INSERT INTO estado (nombre,fecha_creacion) VALUES ('prueba4','2022-03-03');
