1944fa51aad934f7458879329f2cf86fdc4af731
OC18018 <OC18018@ues.edu.sv>
2022-03-31 16:50:50 -0600

    TipoObjetoResource para rest añadido y funcionando

---
+++ src/main/java/sv/edu/ues/occ/ingenieria/tpi135/bachestpi/resources/TipoObjetoResource.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sv.edu.ues.occ.ingenieria.tpi135.bachestpi.resources;
+
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import sv.edu.ues.occ.ingenieria.tpi135.bachestpi.control.TipoObjetoBean;
+import sv.edu.ues.occ.ingenieria.tpi135.bachestpi.entity.TipoObjeto;
+
+/**
+ *
+ * @author armandop444
+ */
+@Path("tipoobjeto")
+@RequestScoped
+public class TipoObjetoResource {
+
+    @Inject
+    TipoObjetoBean toBean;
+
+    @GET
+    @Produces({"application/json; charset=UTF-8"})
+    public Response findAll() {
+        List<TipoObjeto> registros = toBean.findAll();
+        Long total = toBean.contar();
+
+        return Response.ok(registros)
+                .header("Total-Registro", total)
+                .build();
+
+    }
+
+}
