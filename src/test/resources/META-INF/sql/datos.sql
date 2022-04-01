53fb068b3eb6e2271ef473de033ef1f2e386d7d1
OC18018 <OC18018@ues.edu.sv>
2022-03-31 20:55:22 -0600

    persistence de test cambio de drop-create, datos.sql añadido datos de tipoObjeto

--- src/test/resources/META-INF/sql/datos.sql
+++ src/test/resources/META-INF/sql/datos.sql
@@ -2,3 +2,6 @@ INSERT INTO estado (nombre,fecha_creacion) VALUES ('prueba','2022-03-01');
 INSERT INTO estado (nombre,fecha_creacion) VALUES ('prueba2','2022-03-02');
 INSERT INTO estado (nombre,fecha_creacion) VALUES ('prueba3','2022-03-03');
 INSERT INTO estado (nombre,fecha_creacion) VALUES ('prueba4','2022-03-03');
+INSERT INTO tipo_objeto (activo, fecha_creacion) VALUES(true,'2022-03-29');
+INSERT INTO tipo_objeto (activo, fecha_creacion) VALUES(true,'2022-03-30');
+INSERT INTO tipo_objeto (activo, fecha_creacion) VALUES(true,'2022-03-31');
